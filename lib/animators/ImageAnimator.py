a145480a51b4db03d0600a5bc42578c1d92b46bc
Stefan Nožinić <stefan@lugons.org>
2015-07-03 03:52:41 +0200

    Optimize ImageAnimator

--- lib/animators/ImageAnimator.py
+++ lib/animators/ImageAnimator.py
@@ -4,19 +4,14 @@ from .BaseAnimator import *
 import matplotlib.pyplot as plt 
 import numpy as np 
 class ImageAnimator(BaseAnimator):
-    maxDensity = 10.
     def start(self, simulator):
         pass
 
     def update(self, p, v, b, t):
         n,m = p.shape
+        maxDensity = 10.
         mat = np.zeros([n,m,3])
-        for i in range(n):
-            for j in range(m):
-                if b[i][j]:
-                    mat[i][j][1] = 1.0
-                else:
-                    mat[i][j][2] = p[i][j] / self.maxDensity 
-                    if mat[i][j][2] > self.maxDensity:
-                        mat[i][j][2] = self.maxDensity
+        mat[:,:,1] = b
+        mat[:,:,2] = np.clip(p, 0, maxDensity) / maxDensity
+        mat[:,:,2] *= np.ones(b.shape) - b # do not color boundaries to blue 
         plt.imsave("figure-" + str(t) + ".png", mat)
