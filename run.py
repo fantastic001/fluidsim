58992a3fd47a632b56dc0677f0af58fd4640b049
Stefan Nožinić <stefan@lugons.org>
2015-07-15 10:13:36 +0200

    Modifying boundaries

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ animator_class = animator_router.route(sys.argv[7])
 
 def v_func(x, y):
     #if (x-m/2)**2 + (y-n/2)**2 <= 25**2:
-    if x > 2 and x < m-2 and y>2 and y<n-2:
+    if x > 2 and x < m-2-50 and y>2 and y<n-2:
         return np.array([velocity_x, velocity_y])
     else: 
         return np.array([0,0])
@@ -34,8 +34,8 @@ def v_func(x, y):
 def b_func(x,y):
     if x == 2 or x == m-2 or y == 2 or y==n-2:
         return 1.0
-    #if (x - m/2)**2 + (y-n/2)**2 <= 8**2 or (x==int(0.1*m) or x==int(0.9*m) or y==int(0.1*n) or y==int(0.9*n)):
-    #    return True
+    if (x - m/2)**2 + (y-n/2)**2 <= 8**2 or (x==int(0.1*m) or x==int(0.9*m) or y==int(0.1*n) or y==int(0.9*n)):
+        return True
     return 0.0
 
 p = np.zeros([n,m])
