2c5e42a75601c630b3a775c3023fd429d44b7b2b
Stefan Nožinić <stefan@lugons.org>
2015-07-17 12:50:24 +0200

    Implementing step in CFDSimulator without singularities and better handling of laplacian

--- lib/simulators/CFDSimulator.py
+++ lib/simulators/CFDSimulator.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import numpy.linalg 
 
 from .BaseSimulator import BaseSimulator
 
@@ -45,21 +46,26 @@ class CFDSimulator(BaseSimulator):
         res[:,:,1] = dqxx + dqyy
         return res 
 
+    def is_edge(self, i,j):
+        return [i == 0, j == self.m, i == self.n, j == 0]
+
+    def is_out(self,i,j):
+        return i < 0 or i == self.n or j < 0 or j == self.m
+
     def start(self):
-        self.h = 0.001
-        self.old = self.velocities
-        try:
-            self.forces 
-        except NameError:
-            self.forces = np.zeros([self.n, self.m])
-    
+        self.h = 0.1
+        self.n, self.m = (self.velocities.shape[0]*self.h, self.velocities.shape[1]*self.h)
+        self.forces = np.zeros([int(self.n/self.h), int(self.m/self.h), 2]) # Will be removed and modeled differently
+        self.old = self.velocities.copy()
+
     def finish(self):
         pass
 
     def step(self, dt):
         # Setting some stuff for later computation
         #self.old = self.velocities.copy()
-        y,x = np.mgrid[0:100:h, 0:100:h]
+        h = self.h
+        y,x = np.mgrid[0:self.n:h, 0:self.m:h]
         ax = np.arange(0, self.m, h)
         ay = np.arange(0, self.n, h)
 
@@ -67,70 +73,118 @@ class CFDSimulator(BaseSimulator):
         w1 = w0 + dt * self.forces 
         w2 = w1.copy()
         # compute velocity at past 
-        w1p0 = interpolate.RectBivariateSpline(ax, ay, self.old[:,:,0])
-        w1p1 = interpolate.RectBivariateSpline(ax, ay, self.old[:,:,1])
-        for i in ax:
-            for j in ay:
+        w1p0 = interpolate.RectBivariateSpline(ay, ax, self.old[:,:,0])
+        w1p1 = interpolate.RectBivariateSpline(ay, ax, self.old[:,:,1])
+        for j in ax:
+            for i in ay:
                 i_ = i - dt*w1[i,j, 0]
                 j_ = j - dt*w1[i,j, 1]
                 
                 # we do not want to go outside of the boundary 
                 i_ = np.clip(i_, 0, self.m)
                 j_ = np.clip(i_, 0, self.n)
+                
                 w2[i,j,0] = w1p0(i_, j_)
                 w2[i,j,1] = w1p1(i_, j_)
-        size = self.n * self.m 
+        size = int(self.n/h) * int(self.m / h)
         
         # calculating diffusion 
-        A = scipy.sparse.dok_matrix((size, size))
-        for ix in ax:
-            for iy in ay:
-                s = self.m * iy + ix 
-                s1 = self.m*(iy + h) + ix 
-                s2 = self.m*iy + ix+h
-                s3 = self.m*(iy - h) + ix 
-                s4 = self.m*iy + ix - h
-                A[s][s] = 1 - 4/(h**2)
-                
-                # s1
-                if iy + h == self.n-1:
-                    A[s][s1] = 0 
-                else:
-                    A[a][s1] = 1/h**2
+        A = scipy.sparse.csc_matrix((size, size))
+        """
+        for iy in ay:
+            for ix in ax:
+                s = self.m * iy/h + ix/h 
+                s1 = self.m*(iy/h + 1) + ix/h
+                s2 = self.m*iy/h + (ix/h)+1
+                s3 = self.m*(iy/h - 1) + ix/h
+                s4 = self.m*iy/h + (ix/h) - 1
+                s = int(s)
+                s1 = int(s1)
+                s2 = int(s2)
+                s3 = int(s3) 
+                s4 = int(s4)
 
-                # s2 
-                if ix + h == self.m - 1:
-                    A[s][s2] = 0
+                iix, iiy = (int(ix/h), int(iy/h))
+                if (ix, iy) in [(0,0), (0, self.n-1), (self.m-1, 0), (self.m-1, self.n-1)]:
+                    A[s,s] = 1 + 2/(h**2)
+                    if (ix, iy) == (0,0):
+                        A[s, s2] = -1/(h**2)
+                        A[s, s3] = -1/(h**2)
+                    if (ix, iy) == (0,self.m-1):
+                        A[s, s4] = -1/(h**2)
+                        A[s, s3] = -1/(h**2)
+                    if (ix, iy) == (self.n-1,0):
+                        A[s, s2] = -1/(h**2)
+                        A[s, s1] = -1/(h**2)
+                    if (ix, iy) == (self.n-1, self.m-1):
+                        A[s, s1] = -1/(h**2)
+                        A[s, s4] = -1/(h**2)
+                elif iy == 0:
+                    A[s, s2] = -1/h**2
+                    A[s, s3] = -1/h**2
+                    A[s, s4] = -1/h**2
+                    A[s, s] = 1 + 3/(dt * self.viscosity*h**2)
+                elif iy == self.n-1:
+                    A[s, s1] = -1/h**2
+                    A[s, s2] = -1/h**2
+                    A[s, s4] = -1/h**2
+                    A[s, s] = 1 + 3/(dt*self.viscosity * h**2)
+                elif ix == 0:
+                    A[s, s1] = -1/h**2
+                    A[s, s2] = -1/h**2
+                    A[s, s3] = -1/h**2
+                    A[s, s] = 1 + 3/(dt*self.viscosity * h**2)
+                elif ix == self.m-1:
+                    A[s, s1] = -1/h**2
+                    A[s, s3] = -1/h**2
+                    A[s, s4] = -1/h**2
+                    A[s, s] = 1 + 3/(dt*self.viscosity * h**2)
+                #elif ix == 0 or ix == self.m-1 or self.boundaries[iiy, iix]:
+                #    A[s, s1] = -1/h**2
+                #    A[s, s3] = -1/h**2
+                #    A[s,s] = 1 + 2/(dt* self.viscosity * h**2)
                 else:
-                    A[s][s2] = 1/h**2
-
-                # s3 
-                if iy - h == 0:
-                    A[s][s3] = 0
-                else:
-                    A[s][s3] = 1/h**2
-
-                # s4 
-                if ix - h == 0:
-                    A[s][a4] = 0
-                else:
-                    A[s][s4] = 1/h**2
-        
+                    A[s, s1] = -1/h**2
+                    A[s, s2] = -1/h**2
+                    A[s, s3] = -1/h**2
+                    A[s, s4] = -1/h**2
+                    A[s, s] = 1 + 4/(dt * self.viscosity *h**2)
+        """
+        ne_conditions = [np.array([0,1]), np.array([0,-1]), np.array([1, 0]), np.array([-1, 0])]
+        for ix1 in ax:
+            for iy1 in ay:
+                iix1 = int(ix1/h)
+                iiy1 = int(iy1/h)
+                s = (self.m * iiy1) + iix1
+                A[s,s] = -4
+                edges = self.is_edge(iiy1, iix1)
+                for edge in edges:
+                    if edge:
+                        A[s, s] = A[s,s] + 1
+                for condition in ne_conditions:
+                    iiy2 = iiy1 + condition[1]
+                    iix2 = iix1 + condition[0]
+                    if not self.is_out(iiy2, iix2):
+                        s2 = (self.m * iiy2) + iix2
+                        A[s, s2] = 1
+                        A[s2, s] = 1
+        I = scipy.sparse.eye(size)
+        # DEBUG A is singular 
         # calculating w3 
         w2_x = w2[:,:,0].reshape(size)
         w2_y = w2[:,:,1].reshape(size)
-        w30 = scipy.sparse.linalg.spsolve(A, w2_x)
-        w31 = scipy.sparse.linalg.spsolve(A, w2_y)
-
-        w3 = np.zeros([self.n, self.m, 2])
-        w3[:,:,0] = w30.reshape([self.n, self.m])
-        w3[:,:,1] = w31.reshape([self.n, self.m])
+        w30 = scipy.sparse.linalg.spsolve(I - (self.viscosity * dt * h**2)*A, w2_x)
+        w31 = scipy.sparse.linalg.spsolve(I - (self.viscosity * dt * h**2)*A, w2_y)
+        w3 = np.zeros([self.n/h, self.m/h, 2])
+        w3[:,:,0] = w30.reshape([self.n/h, self.m/h])
+        w3[:,:,1] = w31.reshape([self.n/h, self.m/h])
 
         # OH YEAH ! 
-        # We have w3, now we can compute pressure, finally 
+        # i have w3, now I can compute pressure, finally 
         # Qp = div_w3
         div_w3 = self.compute_divergence(w3, h, h)
         div_w3_reshaped = div_w3.reshape(size)
+        """
         Q = scipy.sparse.dok_matrix((size, size))
         for ix in ax:
             for iy in ay:
@@ -139,25 +193,32 @@ class CFDSimulator(BaseSimulator):
                 s2 = self.m*iy + ix+h
                 s3 = self.m*(iy - h) + ix 
                 s4 = self.m*iy + ix - h
+                # these are not indices yet, divide by h
+                s = int(s/h)
+                s1 = int(s1/h)
+                s2 = int(s2/h)
+                s3 = int(s3/h) 
+                s4 = int(s4/h)
                 #Q[s][s] = -4/(h**2)
                 if (ix, iy) in [(0, 0), (0, self.n), (self.m, 0), (self.m, self.n)]:
-                    Q[s][s] = 0
+                    Q[s, s] = 0
                 elif ix + h == self.m or self.boundaries[iy, ix + h] or ix - h < 0 or self.boundaries[iy, ix-h]:
-                    Q[s][s] = -2/(h**2)
-                    Q[s][s1] = 1/h**2
-                    Q[s][s3] = 1/h**2
+                    Q[s, s] = -2/(h**2)
+                    Q[s, s1] = 1/h**2
+                    Q[s, s3] = 1/h**2
                 elif iy + h == self.n or self.boundaries[iy + h, ix] or iy - h < 0 or self.boundaries[iy-h, ix]:
-                    Q[s][s2] = 1/h**2
-                    Q[s][s4] = 1/h**2
-                    Q[s][s] = -2/(h**2)
+                    Q[s, s2] = 1/h**2
+                    Q[s, s4] = 1/h**2
+                    Q[s, s] = -2/(h**2)
                 else:
-                    Q[s][s] = -4/(h**2)
-                    Q[s][s1] = 1/h**2 
-                    Q[s][s2] = 1/h**2
-                    Q[s][s3] = 1/h**2
-                    Q[s][s4] = 1/h**2
-        p_ = scipy.sparse.linalg.spsolve(Q, div_w3_reshaped)
-        p = p_.reshaped(self.n, self.m)
+                    Q[s, s] = -4/(h**2)
+                    Q[s, s1] = 1/h**2 
+                    Q[s, s2] = 1/h**2
+                    Q[s, s3] = 1/h**2
+                    Q[s, s4] = 1/h**2
+        """
+        p_ = scipy.sparse.linalg.spsolve(A / h**2, div_w3_reshaped)
+        p = p_.reshape(self.n/h, self.m/h)
         grad_p = self.compute_gradient(p, h, h)
         w4 = w3 - grad_p 
         self.velocities = w4 
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 
 from lib import Router
 from lib.animators import *
-from lib.simulators import LBMSimulator 
+from lib.simulators import *
 from lib.draw import * 
 
 import sys
@@ -11,6 +11,7 @@ if sys.argv[1] == "--help":
     print("this width height velocity_x velocity_y num_iterations viscosity animator")
     exit(0)
 
+h = 0.1
 m,n = int(sys.argv[1]), int(sys.argv[2])
 velocity_x, velocity_y = float(sys.argv[3]), float(sys.argv[4])
 num_iters = int(sys.argv[5])
@@ -26,26 +27,28 @@ animator_class = animator_router.route(sys.argv[7])
 
 def v_func(x, y):
     #if (x-m/2)**2 + (y-n/2)**2 <= 25**2:
-    if x > 2 and x < m-2 and y>2 and y<n-2:
-        return np.array([velocity_x, velocity_y])
-    else: 
+    #if x > 2 and x < m-2 and y>2 and y<n-2:
+    #    return np.array([velocity_x, velocity_y])
+    #else: 
         return np.array([0,0])
 
 def b_func(x,y):
-    if x == 2 or x == m-2 or y == 2 or y==n-2:
-        return 1.0
+    #if x == 2 or x == m-2 or y == 2 or y==n-2:
+    #    return 1.0
     #if (x - m/2)**2 + (y-n/2)**2 <= 8**2 or (x==int(0.1*m) or x==int(0.9*m) or y==int(0.1*n) or y==int(0.9*n)):
     #    return True
     return 0.0
 
-p = np.zeros([n,m])
-p.fill(5.0)
+p = np.zeros([n/h,m/h])
+p.fill(0.0)
 
-v = np.zeros([n,m, 2])
-draw_from_function(v, n, m, v_func)
-b = np.zeros([n,m])
-draw_from_function(b,n,m, b_func)
+N,M = (int(n/h), int(m/h))
 
-simulator = LBMSimulator(p,v,b, viscosity)
+v = np.zeros([N,M, 2])
+draw_from_function(v, N, M, v_func)
+b = np.zeros([N,M])
+draw_from_function(b,N,M, b_func)
+
+simulator = CFDSimulator(p,v,b, viscosity)
 animator = animator_class(simulator) 
 animator.run(num_iters)
