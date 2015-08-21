2aa2812c0b14430f9ad6d8d790a1c8eb8b80f00d
Stefan Nožinić <stefan@lugons.org>
2015-08-21 09:16:07 +0200

    Better scaling control in FieldAnimator

--- lib/animators/FieldAnimator.py
+++ lib/animators/FieldAnimator.py
@@ -11,6 +11,7 @@ import os.path
 class FieldAnimator(BaseAnimator):
     def start(self, simulator, **kwargs):
         self.target_path = kwargs.get("target_path", "frames/")
+        self.scale = kwargs.get("scale", 100)
 
         if not os.path.isdir(self.target_path):
             os.mkdir(self.target_path)
@@ -18,8 +19,9 @@ class FieldAnimator(BaseAnimator):
     def update(self, p, v, b, t):
         print("Iteration: %d" % t) 
         n,m = p.shape
+        f = 100*v
         mat = np.zeros([n,m,3])
-        plt.quiver(v[::10,::10, 0], v[::10,::10,1], units="width")
+        plt.quiver(v[::10,::10, 0], v[::10,::10,1], scale=self.scale)
         plt.savefig(self.target_path + "/field-%d.png" % t)
         plt.clf()
 
