bb55a795531f7cd0075bf63b28575235bdaa0dd9
Stefan Nožinić <stefan@lugons.org>
2015-09-30 02:31:14 +0200

    Changing explicit advection to ensure stability at some small time steps

--- lib/simulators/CFDExplicitSimulator.py
+++ lib/simulators/CFDExplicitSimulator.py
@@ -18,21 +18,29 @@ class CFDExplicitSimulator(CFDSimulator):
     def advection_primitive(self, u):
         u_x = u[:,:,0]
         u_y = u[:,:,1]
-        duxdx = self.compute_gradient(u[:,:,0], self.h, self.h)[:,:,0]
-        duydx = self.compute_gradient(u[:,:,1], self.h, self.h)[:,:,0]
-        duxdy = self.compute_gradient(u[:,:,0], self.h, self.h)[:,:,1]
-        duydy = self.compute_gradient(u[:,:,1], self.h, self.h)[:,:,1]
+        duxdx = self.compute_gradient(
+            u[:,:,0], self.h, self.h,
+            #edge_order=1
+        )[:,:,0]
+        duydx = self.compute_gradient(
+            u[:,:,1], self.h, self.h,
+            #edge_order=1
+        )[:,:,0]
+        duxdy = self.compute_gradient(
+            u[:,:,0], self.h, self.h,
+            #edge_order=1
+        )[:,:,1]
+        duydy = self.compute_gradient(
+            u[:,:,1], self.h, self.h,
+            #edge_order=1
+        )[:,:,1]
         
-        dudx = u.copy()
-        dudy = u.copy()
-        dudx[:,:,0] = duxdx
-        dudx[:,:,1] = duydx
-        dudy[:,:,0] = duxdy
-        dudy[:,:,1] = duydy
-        dudy = np.array([duxdy, duydy])
         s = u.copy()
         s[:,:,0] = u_x * duxdx + u_y * duxdy
         s[:,:,1] = u_x * duydx + u_y * duydy
+        
+        s[[0, -1], :, :] = 0
+        s[:, [0, -1], :] = 0
         return s
     
     def perform_advection(self, w1, dt):
